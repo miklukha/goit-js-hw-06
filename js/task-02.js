91a59fa4a60ce4ed3e0ba8632182e15dc4c0167d
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-02 11:39:43 +0200

    do task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listOfIngredientsRef = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.textContent = ingredient;
+  item.classList.add('item');
+
+  return item;
+});
+
+listOfIngredientsRef.append(...items);
