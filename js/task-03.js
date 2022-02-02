fbd2455cff933e319ddd74e4340affbd7ff1b6e0
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-02 12:28:01 +0200

    do task3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryRef = document.querySelector('.gallery');
+
+const imageInItem = images
+  .map(image => `<li><img src="${image.url}" alt="${image.alt}"></li>`)
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', imageInItem);
