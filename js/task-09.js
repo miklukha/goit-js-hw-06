97a86d80d6909f6df2c73c96efeea138ed7ca4e2
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-03 13:25:03 +0200

    do task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const bodyRef = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorText = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeColor);
+
+function onChangeColor() {
+  const hex = getRandomHexColor();
+  bodyRef.style.backgroundColor = hex;
+  colorText.textContent = hex;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
