5cc77ded3f95358047bf267eb93b7855aff44f04
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-03 13:08:02 +0200

    do task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const [email, password] = event.currentTarget.elements;
+
+  if (!email.value || !password.value) {
+    return alert('All fields must be filled in');
+  }
+
+  const dataForm = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log(dataForm);
+
+  // const formData = new FormData(event.currentTarget);
+  // const formDataObj = {};
+
+  // formData.forEach((value, name) => {
+  //   formDataObj[name] = value;
+  // });
+
+  // console.log(formDataObj);
+
+  form.reset();
+}
