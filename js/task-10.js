22e536f7da3c28563eacbe39850fa390d318fa8b
Anna Miklukha <miklukha.anna@ukr.net>
2022-02-03 16:14:02 +0200

    do task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,36 @@
+const inputNumber = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const divCollection = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = inputNumber.value;
+  createBoxes(amount);
+  inputNumber.value = '';
+});
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const arrayOfDiv = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+
+    const hex = getRandomHexColor();
+    divEl.style.width = `${30 + i * 10}px`;
+    divEl.style.height = `${30 + i * 10}px`;
+    divEl.style.backgroundColor = hex;
+
+    arrayOfDiv.push(divEl);
+  }
+
+  divCollection.append(...arrayOfDiv);
+}
+
+function destroyBoxes() {
+  divCollection.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
